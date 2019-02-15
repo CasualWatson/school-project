d360d655ec6dd8a4624b9c97bfdd0f143a89b9e2
CasualWatson <i3watson@tuta.io>
2019-02-14 21:34:41 -0600

    Button Highlight Setup

--- School Project Beat em' UpPlatformer/Assets/Scripts/TitleMenuScript.cs
+++ School Project Beat em' UpPlatformer/Assets/Scripts/TitleMenuScript.cs
@@ -6,20 +6,13 @@ using UnityEngine.UI;
 public class TitleMenuScript : MonoBehaviour
 {
     public Camera mainCamera;
-    public GameObject textPrefab;
+    public Canvas canvas;
     private List<GameObject> textList = new List<GameObject>();
 
 	// Use this for initialization
 	void Start ()
     {
-        var spacing = 25;
-		// create three of the text objects and align them properly
-        for (int i = 0; i < 3; i++)
-        {
-            var newText = Instantiate(textPrefab);
-            newText.transform.position = new Vector2(mainCamera.scaledPixelWidth / 2, mainCamera.scaledPixelHeight + (spacing * i));
-            newText.GetComponent<Text>().text = "This is Object #: " + i;
-        }
+
 	}
 	
 	// Update is called once per frame
---
+++ School Project Beat em' UpPlatformer/Assets/TitleTextScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleTextScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void ColorChange()
+    {
+        gameObject.GetComponent<Image>().color = Color.red;
+    }
+    public void ResetColor()
+    {
+        gameObject.GetComponent<Image>().color = Color.white;
+    }
+}
